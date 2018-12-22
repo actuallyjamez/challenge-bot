37358fcd70fcb01b09f43244689f6ee3cfdc347f
Pim Schwippert <15077675@student.hhs.nl>
2018-12-22 21:45:18 +0100

    This is the new challenge command stuff woop

--- commands/challenge.js
+++ commands/challenge.js
@@ -1,42 +1,91 @@
 exports.run = (client, message, args) => {
-    const {RichEmbed, MessageCollector} = require('discord.js');
-    const collector = new MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 15000 });
+    const { RichEmbed, MessageCollector } = require('discord.js');
+    let collector = new MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 15000, maxMatches: 1 });
+
+    const minTitleLength = 1;
+    const maxTitleLength = 50;
+
+    const minDescLength = 1;
+    const maxDescLength = 1000;
+
     message.channel.send("Please enter a title");
 
-    let title = '';
 
     collector.on('collect', message => {
-        title = message;
+        const title = message.content;
+
+        if (checkInput(message, title, 'Title', minTitleLength, maxTitleLength)) {
+            return;
+        }
+
+        message.channel.send("Please enter a description of the challenge");
+
+        collector = new MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 25000, maxMatches: 1 });
+
+        collector.on('collect', message => {
+            const description = message.content;
+            console.log(description);
+            if (checkInput(message, description, 'Description', minDescLength, maxDescLength)) {
+                return;
+            }
+
+            //TODO: could be moved? Maybe somewhere else where to have different templates
+            const embedDescription =
+                `*${description}* \n
+             Starting time: 00:00
+             End Time: 00:00 \n
+             Please react with a ✅ to join this challenge. You will be notified through DMs when the challenge starts!
+             @Challenged`
+            const embed = new RichEmbed()
+                .setTitle(`Challenge: ${title}`)
+                .setColor(0xff0000)
+                .setDescription(`${embedDescription}`);
+
+            message.channel.send(embed)
+                .then(function (message) {
+                    message.react("✅");
+                }).catch(function () {
+                    message.channel.send("oops something went wrong!");
+                    console.log("Rejected promise in embedded message");
+                });
+        });
     });
+};
 
-    if (title.length < 1) {
-        message.channel.send("You need to specify a title");
-        return;
+function checkInput(message, input, type, minLength, maxLength) {
+    let inError = false;
+    if (input.length === 0) {
+        message.channel.send(HandleError(ErrorCodes.ERROR_NOT_DEFINED, type));
+        inError = true;
     }
-    if(title.length > 50)
-    {
-        message.channel.send("Title is too long! Please shorten it");
-        return;
+    if (input.length < minLength) {
+        message.channel.send(HandleError(ErrorCodes.ERROR_TOO_SHORT, type));
+        inError = true;
     }
-    
-    // const combinedArgs = args.join(' '); 
-    // const split = combinedArgs.split(',');
-    // const title = split[0];
-    
-    // if(title.length > 50){
-    //     message.channel.send("Title is too long! Please shorten it");
-    //     return;
-    // }
+    if (input.length > maxLength) {
+        message.channel.send(HandleError(ErrorCodes.ERROR_TOO_LONG, type));
+        inError = true;
+    }
+    return inError;
+}
 
-    const embed = new RichEmbed()
-    .setTitle(`Challenge ${title}`)
-    .setColor(0xff0000)
-    .setDescription('beep boop this is a test');
-    message.channel.send(embed);
+function HandleError(errorCode, type) {
+    switch (errorCode) {
+        case ErrorCodes.ERROR_NOT_DEFINED:
+            return `You need to specify a ${type.toLowerCase()}`;
+        case ErrorCodes.ERROR_TOO_SHORT:
+            return `Your ${type.toLowerCase()} is too short`;
+        case ErrorCodes.ERROR_TOO_LONG:
+            return `${uppercaseFirstLetter(type)} is too long! Please shorten it`;
+    }
+}
 
-    console.log(args);
+function uppercaseFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + type.slice(1);
+}
 
-
-    //////////////////////////////////////////////////////////////////////////////////////////
-};  ////got my version working, implemented both of our ideas, both fall back on eachother////
-    //////////////////////////////////////////////////////////////////////////////////////////
+const ErrorCodes = {
+    ERROR_NOT_DEFINED: 0,
+    ERROR_TOO_SHORT: 1,
+    ERROR_TOO_LONG: 2,
+}
